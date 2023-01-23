8aad983daef4e78fd27a65930f0137ddd4e05337
Michael Gastello <michaelgastello@aol.com>
2023-01-23 17:33:34 -0500

    my flatten

--- enumerables.rb
+++ enumerables.rb
@@ -52,8 +52,51 @@ class Array
 # p a.my_reject { |num| num > 1 } # => [1]
 # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
+    def my_any?(&prc)
+        i = 0
+        while i < self.length
+            if prc.call(self[i])
+                return true
+            end
+            i += 1
+        end
 
+        false
+    end
 
+    def my_all?(&prc)
+        i = 0
+        while i < self.length
+            if prc.call(self[i]) == false
+                return false
+            end
+            i += 1
+        end
+        true
+    end
+    # a = [1, 2, 3]
+    # p a.my_any? { |num| num > 1 } # => true
+    # p a.my_any? { |num| num == 4 } # => false
+    # p a.my_all? { |num| num > 1 } # => false
+    # p a.my_all? { |num| num < 4 } # => true
+
+    def my_flatten
+        if self.is_a?(Array) == false
+            return [self]
+        end
+        arr = []
+
+        i = 0
+        while i < self.length
+            if self.is_a?(Array) == false
+                arr << self[i]
+            else
+                my_flatten(self[i])
+            end
+        end
+
+        arr
+    end
 end
 
-
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
